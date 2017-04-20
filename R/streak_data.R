0c537fa538c665d49b890bfc13ac2544673b3ccb
Jim Albert <albertcb1@gmail.com>
2017-04-20 18:52:10 -0400

    edit function

--- R/streak_data.R
+++ R/streak_data.R
@@ -1,10 +1,12 @@
-streak_data <- function(pbpdata, pid, eventcode){
+streak_data <- function(pbpdata, pid, eventcode, AB=FALSE){
   pbpdata <- mutate(pbpdata,
                       Date=ymd(str_sub(GAME_ID, 4, 11)),
                       Game=str_sub(GAME_ID, 12, 12))
   filter(pbpdata, 
            BAT_ID == pid, BAT_EVENT_FL == TRUE) %>%
       arrange(Date, Game) -> d
+  if(AB==TRUE)
+    d <- filter(d, AB_FL==TRUE)
     
   ifelse(d$EVENT_CD %in% eventcode, 1, 0)
 }
